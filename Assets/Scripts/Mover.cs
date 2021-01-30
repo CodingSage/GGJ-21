0716b25acb36d3539e21bf95d946db6ea0563183
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2021-01-30 14:29:00 -0500

    added constant velocity movement feature to player'

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,19 +7,12 @@ public class Mover : MonoBehaviour
 {
     public float accelaration = 5f;
     public float maxSpeed = 10f;
-    public float velocity = 5f;
 
     private Rigidbody2D body;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-    }
-
-    public void MoveWithConstantVelocity(Vector2 direction)
-    {
-        direction = Vector3.Normalize(direction);
-        body.velocity = direction * velocity;
     }
 
     public void AccelarateInDirection(Vector2 direction)
@@ -29,10 +22,5 @@ public class Mover : MonoBehaviour
         newVelocity.x = Mathf.Clamp(newVelocity.x, -maxSpeed, maxSpeed);
         newVelocity.y = Mathf.Clamp(newVelocity.y, -maxSpeed, maxSpeed);
         body.velocity = newVelocity;
-    }
-
-    public void StopMovement()
-    {
-        body.velocity = Vector2.zero;
     }
 }
