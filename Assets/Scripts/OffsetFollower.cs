18a730f980483c7ddf9db4a131fb9d2d96c747f5
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2021-01-29 23:28:24 -0500

    Added basic level elements - player, ground, following camera (#2)
    * basic project folders
    
    * added basic level elements

---
+++ Assets/Scripts/OffsetFollower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffsetFollower : MonoBehaviour
+{
+    public Transform target;
+    private float yOffset;
+    private float xOffset;
+
+    private void Start()
+    {
+        yOffset = transform.position.y - target.position.y;
+        xOffset = transform.position.x - target.position.x;
+    }
+
+    private void Update()
+    {
+        Vector3 targetPosition = target.position;
+        Vector3 updatedPosition = new Vector3(targetPosition.x + xOffset, targetPosition.y + yOffset, transform.position.z);
+        transform.position = updatedPosition;
+    }
+}
