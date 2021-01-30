37e350de52b6f1bb73d35d9f258f11d87fdd567a
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2021-01-30 14:22:24 -0500

    Beach ball enemy controller (#7)
    * Added Script
    
    * Jumping Beach Ball Enemy
    
    Co-authored-by: davidkwhitlock <dwhitlock@shuntley.co>

---
+++ Assets/Scripts/GroundDetector.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class GroundDetector : MonoBehaviour
+{
+    [HideInInspector] //you can't mess with this in inspector even if it's public
+
+    public bool onGround = false;
+
+    private Animator animator;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        onGround = true;
+
+        if (animator)
+        {
+            animator.SetBool("OnGround", true);
+        }
+
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        onGround = false;
+
+        if (animator)
+        {
+            animator.SetBool("OnGround", false);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/JumpingEnemyController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(GroundDetector))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Jumper))]
+
+
+public class JumpingEnemyController : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody2D;
+    private GroundDetector groundDetector;
+    private Jumper jumper;
+    private Animator animator;
+    float lastTimeJumped;
+
+    public float jumpForce = 6f;
+    private bool hasJumped = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+        groundDetector = GetComponent<GroundDetector>();
+        animator = GetComponent<Animator>();
+        jumper = GetComponent<Jumper>();
+    }
+
+    public void Jump()
+    {
+
+        lastTimeJumped = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Mathf.Floor(Time.time) % 3 == 0)
+        {
+            if (hasJumped == false)
+            {
+                myRigidBody2D.velocity += new Vector2(0f, jumpForce);
+                hasJumped = true;
+            }
+            animator.SetTrigger("EnemyJump");
+
+
+        }
+
+        else
+        {
+            hasJumped = false;
+        }
+
+    }
+}
