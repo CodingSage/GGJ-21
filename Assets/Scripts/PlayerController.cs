49be497eaf0c9fa9ec20316b050c966365258ac3
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2021-01-29 23:27:24 -0500

    added basic level elements

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Mover))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class PlayerController : MonoBehaviour
+{
+    private Mover mover;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        mover = GetComponent<Mover>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            mover.AccelarateInDirection(-Vector2.right);
+            spriteRenderer.flipX = true;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            mover.AccelarateInDirection(Vector2.right);
+            spriteRenderer.flipX = false;
+        }
+    }
+}
