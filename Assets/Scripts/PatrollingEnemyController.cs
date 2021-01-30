687ca77ce28003c6f510ca63b6886e17fe210cc5
shuntley <36647176+shuntley@users.noreply.github.com>
2021-01-30 17:03:28 -0500

    Crab enemy (#12)
    * Crab Enemy
    
    * Enemy Prefabs done
    
    Co-authored-by: davidkwhitlock <dwhitlock@shuntley.co>

---
+++ Assets/Scripts/PatrollingEnemyController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrollingEnemyController : MonoBehaviour
+{
+    public float speed = 1f;
+    public bool MoveRight;
+
+    public void Start()
+    {
+        StartCoroutine(Patroll());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(MoveRight == true)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+    }
+
+    public IEnumerator Patroll()
+    {
+        MoveRight = true;
+        yield return new WaitForSeconds(1f);
+        MoveRight = false;
+        yield return new WaitForSeconds(1f);
+        MoveRight = true;
+        yield return new WaitForSeconds(1f);
+        MoveRight = false;
+        yield return new WaitForSeconds(1f);
+        MoveRight = true;
+        yield return new WaitForSeconds(1f);
+        MoveRight = false;
+        yield return new WaitForSeconds(1f);
+        MoveRight = true;
+        yield return new WaitForSeconds(1f);
+        MoveRight = false;
+        yield return new WaitForSeconds(1f);
+        MoveRight = true;
+        yield return new WaitForSeconds(1f);
+        MoveRight = false;
+        yield return new WaitForSeconds(1f);
+        MoveRight = true;
+        yield return new WaitForSeconds(1f);
+        MoveRight = false;
+        yield return new WaitForSeconds(1f);
+
+    }
+}
