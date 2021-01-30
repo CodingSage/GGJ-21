0603bff91d3d619804c18d87d3102c11b5914247
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2021-01-30 14:59:02 -0500

    Added game controller and speech system (#8)
    * added constant velocity movement to mover
    
    * added constant velocity movement feature to player'
    
    * added basic game controller and speech system
    
    * merged with main

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpeechEventSystem))]
+public class GameController : MonoBehaviour
+{
+	public GameObject player;
+	public GoalTrigger goal;
+    
+    private SpeechEventSystem speechSystem;
+
+    void Start() 
+    {
+        if (goal == null || player == null)
+        {
+            throw new MissingComponentException("Game controller components missing");
+        }
+        speechSystem = GetComponent<SpeechEventSystem>();
+    }
+
+    private void Update()
+    {
+        if (goal.GoalReached())
+        {
+            speechSystem.TriggerSpeech(SpeechEvent.LEVEL1);
+        }
+    }
+}
