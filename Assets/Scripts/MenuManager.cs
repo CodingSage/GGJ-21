26064e08c1f2a5b32f20d62a5a514faa702755b2
xxlovelymango <75403684+xxlovelymango@users.noreply.github.com>
2021-01-31 01:07:50 -0500

    Menu
    Made a menu!

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject controlPanel;
+
+    private void Start()
+    {
+        HideControlPanel();
+    }
+
+    public void LoadFirstLevel(){
+         int menuID = SceneManager.GetActiveScene().buildIndex;
+
+
+         SceneManager.LoadScene(menuID + 1);
+        }
+
+    public void ShowControlPanel()
+    {
+        controlPanel.SetActive(true);
+    }
+
+    public void HideControlPanel()
+    {
+        controlPanel.SetActive(false);
+    }
+    
+}
