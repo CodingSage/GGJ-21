735ecad637260398fd1fec5e4f6cf974a7f2dc12
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2021-01-30 19:06:55 -0500

    Player Movement Animated (#21)
    Co-authored-by: davidkwhitlock <dwhitlock@shuntley.co>

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 [RequireComponent(typeof(Mover))]
 [RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     private Mover mover;
     private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
     void Start()
     {
         mover = GetComponent<Mover>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
@@ -20,12 +23,19 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             mover.AccelarateInDirection(-Vector2.right);
+            animator.SetBool("Moving", true);
             spriteRenderer.flipX = true;
         }
         else if (Input.GetKey(KeyCode.D))
         {
             mover.AccelarateInDirection(Vector2.right);
+            animator.SetBool("Moving", true);
             spriteRenderer.flipX = false;
+        }
+
+        if(Input.anyKey == false)
+        {
+            animator.SetBool("Moving", false);
         }
     }
 }
