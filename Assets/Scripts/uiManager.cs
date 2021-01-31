8a095f48676560b549e69f29570097d5e7416b99
shuntley <36647176+shuntley@users.noreply.github.com>
2021-01-31 10:07:47 -0500

    DaveMenu (#33)
    Co-authored-by: davidkwhitlock <dwhitlock@shuntley.co>

---
+++ Assets/Scripts/uiManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class uiManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void EndOne()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+
+    public void EndTwo()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void EndThree()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void EndFour()
+    {
+        SceneManager.LoadScene("Level5");
+    }
+
+    public void EndFive()
+    {
+        SceneManager.LoadScene("DaveMenu");
+    }
+}
