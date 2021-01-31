ab2c409166becaace46749d0d04d4c2e684e5c58
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2021-01-31 04:45:51 -0500

    added moving raft hurdle (#32)

---
+++ Assets/Scripts/RaftController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class RaftController : MonoBehaviour
+{
+    public float speed;
+    public float leftX;
+    public float rightX;
+    private bool goLeft = false;
+
+    void Update()
+    {
+        float currentX = transform.position.x;
+        if (currentX > rightX) goLeft = true;
+        else if(currentX < leftX) goLeft = false;
+
+        float diff = goLeft ? -1 * speed : speed;
+        transform.position = 
+            new Vector3(transform.position.x + diff * Time.deltaTime, transform.position.y, transform.position.z);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}
